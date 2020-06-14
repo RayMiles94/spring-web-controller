0268217d51032a3f9fcb90ffa69daf98eee5920f
RayMiles94 <natsuboy94@gmail.com>
2020-06-14 22:26:07 +0100

    config spring mvc

---
+++ src/main/java/com/springframetutorial/springfirsttutorial/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.springframetutorial.springfirsttutorial.controller;
+
+import com.springframetutorial.springfirsttutorial.repos.BookRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookrepository;
+
+    @RequestMapping(name = "/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookrepository.findAll());
+        return "books";
+    }
+    
+}
