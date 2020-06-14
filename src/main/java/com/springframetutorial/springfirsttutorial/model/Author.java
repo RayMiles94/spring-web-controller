d5015013ed1027425ce5aea2e3f3657f9d7d9f5f
RayMiles94 <natsuboy94@gmail.com>
2020-06-14 21:14:55 +0100

    create jpa for spring

--- src/main/java/com/springframetutorial/springfirsttutorial/model/Author.java
+++ src/main/java/com/springframetutorial/springfirsttutorial/model/Author.java
@@ -3,11 +3,21 @@ package com.springframetutorial.springfirsttutorial.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstname;
     private String lastname;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
     public Author() {}
@@ -21,6 +31,14 @@ public class Author {
         this.firstname = firstname;
         this.lastname = lastname;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstname() {
--- src/main/java/com/springframetutorial/springfirsttutorial/model/Book.java
+++ src/main/java/com/springframetutorial/springfirsttutorial/model/Book.java
@@ -4,20 +4,34 @@ package com.springframetutorial.springfirsttutorial.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Book  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
-    public Book() {}
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"), inverseJoinColumns = @JoinColumn(name="author_id"))
+    private Set<Author> authors = new HashSet<>();
 
+    public Book() {}
     public Book(String title, String isbn, String publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
-
     public Book(String title, String isbn, String publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
@@ -25,7 +39,15 @@ public class Book  {
         this.authors = authors;
     }
 
-    private Set<Author> authors = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
 
     public String getTitle() {
         return title;
@@ -58,9 +80,6 @@ public class Book  {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
-
-    
-
 
 
 }
