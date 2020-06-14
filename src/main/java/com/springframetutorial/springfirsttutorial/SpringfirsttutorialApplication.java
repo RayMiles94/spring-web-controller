6630fed0d85a7483d91b366a4e8232771285ec9c
RayMiles94 <natsuboy94@gmail.com>
2020-06-14 16:30:00 +0100

    init commit

---
+++ src/main/java/com/springframetutorial/springfirsttutorial/SpringfirsttutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springframetutorial.springfirsttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfirsttutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfirsttutorialApplication.class, args);
+	}
+
+}
