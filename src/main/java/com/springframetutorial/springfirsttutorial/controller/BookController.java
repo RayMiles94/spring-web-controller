8d334beb1424b0e5b90afb64da907330d3e41410
RayMiles94 <natsuboy94@gmail.com>
2020-06-14 22:51:35 +0100

    add theamleaf

--- src/main/java/com/springframetutorial/springfirsttutorial/controller/BookController.java
+++ src/main/java/com/springframetutorial/springfirsttutorial/controller/BookController.java
@@ -11,10 +11,16 @@ public class BookController {
 
     private BookRepository bookrepository;
 
+    public BookController(BookRepository bookrepository) {
+        this.bookrepository = bookrepository;
+    }
+    
     @RequestMapping(name = "/books")
     public String getBooks(Model model) {
         model.addAttribute("books", bookrepository.findAll());
         return "books";
     }
+
+    
     
 }
